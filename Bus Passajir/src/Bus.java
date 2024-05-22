7b0783b8c99493def14510152c8bab34f03ca75b
Alexsiekl <Alexsiekl@yandex.ru>
2024-05-22 12:19:06 +0300

    Добавлены класс и поля класса

---
+++ Bus Passajir/src/Bus.java
@@ -0,0 +1,6 @@
+public class Bus {
+    private String mark;
+    private String model;
+    private String klass;
+    private  double lenght;
+}
--- Bus Passajir/src/Main.java
+++ Bus Passajir/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("\t*** Создаём модели автобусов***");
+Bus lliaz = new Bus();
     }
 }
